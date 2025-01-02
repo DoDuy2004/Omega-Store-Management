644158156109575f43a591a26be513bfb6ff10b0
DoDuy2004 <duy2004.gi@gmail.com>
2025-01-02 23:37:32 +0700

    Initial project

---
+++ OmegaStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OmegaStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
