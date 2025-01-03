cb26bb5f383cdef4ebb92376f329e15d4c59cf3b
namnguyxn101 <phuongnam3235@gmail.com>
2025-01-03 10:18:45 +0700

    [Nam] cutted home, roles, acount page

--- OmegaStore/obj/Debug/net8.0/OmegaStore.AssemblyInfo.cs
+++ OmegaStore/obj/Debug/net8.0/OmegaStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmegaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644158156109575f43a591a26be513bfb6ff10b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmegaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmegaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
