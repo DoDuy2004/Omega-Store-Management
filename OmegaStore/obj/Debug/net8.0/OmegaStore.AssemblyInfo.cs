769fc2d76673383ec46714a06583b15055263236
DoDuy2004 <duy2004.gi@gmail.com>
2025-01-04 13:35:04 +0700

    Save changes before merge

--- OmegaStore/obj/Debug/net8.0/OmegaStore.AssemblyInfo.cs
+++ OmegaStore/obj/Debug/net8.0/OmegaStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmegaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b31f22cefae3ca91444c82988c09663fa7aaab08")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmegaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmegaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
