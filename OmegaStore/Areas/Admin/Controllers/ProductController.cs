2d9d836f6e391c444e524c0842f04dcbe1f2ad35
NTPhat004 <ntphat05102004@gmail.com>
2025-01-06 21:08:44 +0700

    [Phat][Load Data Admin Area]

--- OmegaStore/Areas/Admin/Controllers/ProductController.cs
+++ OmegaStore/Areas/Admin/Controllers/ProductController.cs
@@ -1,18 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OmegaStore.Models;
 
 namespace OmegaStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly StoreDbContext _context;
+
+        public ProductController(StoreDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        public IActionResult Detail()
+        //Trang Danh sách sản phẩm
+        public IActionResult Index()
         {
-            return View();
+            var products = _context.Products.ToList(); // Lấy tất cả các sản phẩm từ cơ sở dữ liệu
+            return View(products); // Truyền danh sách sản phẩm vào View
+        }
+
+        //Trang Chi tiết sản phẩm
+        public IActionResult Detail(int id)
+        {
+            var product = _context.Products
+                             .FirstOrDefault(p => p.Id == id); // Lấy sản phẩm theo ProductId
+
+            if (product == null)
+            {
+                return NotFound(); // Nếu sản phẩm không tồn tại, trả về 404
+            }
+
+            return View(product); // Truyền sản phẩm vào View
         }
 
         public IActionResult Edit()
