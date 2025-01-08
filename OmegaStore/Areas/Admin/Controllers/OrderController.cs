910fbcea14c3e60fd9c67e016b48a0d2dbab8bfd
NguyenTu2004-Work <nguyenduythanhtu2004@gmail.com>
2025-01-08 16:33:34 +0700

    [ThanhTu][UpdateOrderStatus]

--- OmegaStore/Areas/Admin/Controllers/OrderController.cs
+++ OmegaStore/Areas/Admin/Controllers/OrderController.cs
@@ -1,15 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OmegaStore.Models;
 
 namespace OmegaStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class OrderController : Controller
     {
+
+        private readonly StoreDbContext _Context;
+        public OrderController(ILogger<HomeController> logger, StoreDbContext StoreDbContext)
+        {
+
+            _Context = StoreDbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            var Order = _Context.Orders.ToList();
+            return View(Order);
         }
+        [HttpPost]
+        public IActionResult UpdateStatus(int orderid, int status)
+        {
 
+            try
+            {
+             
+                var order = _Context.Orders.FirstOrDefault(o => o.Id == orderid);
+                if (order == null)
+                {
+                    return Json(new { success = false, message = "Đơn hàng không tồn tại" });
+                }
+                int st = status;
+                order.Status = status;
+                _Context.SaveChanges();
+    
+
+                return Json(new { success = true, message = $"Trạng thái đơn hàng {orderid} đã được cập nhật.", st = st });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Có lỗi xảy ra: " + ex.Message });
+            }
+        }
+        [HttpPost]
+        public IActionResult DeleteOrder(int orderid, int status)
+        {
+
+            try
+            {
+                var order = _Context.Orders.FirstOrDefault(o => o.Id == orderid);
+                if (order == null)
+                {
+                    return Json(new { success = false, message = "Hủy đơn hàng không thành công" });
+                }
+
+                order.Status = status;
+                _Context.SaveChanges();
+
+                return Json(new { success = true, message = $"Đã hủy đơn hàng {orderid}." });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Có lỗi xảy ra: " + ex.Message });
+            }
+        }
         public IActionResult Detail()
         {
             return View();
