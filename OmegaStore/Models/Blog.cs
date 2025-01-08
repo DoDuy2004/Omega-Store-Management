813988e8402dd790afa3d98a9458aae1b2f4c74c
NTPhat004 <ntphat05102004@gmail.com>
2025-01-08 20:34:52 +0700

    [Phat][Blog Remake]

--- OmegaStore/Models/Blog.cs
+++ OmegaStore/Models/Blog.cs
@@ -8,10 +8,13 @@ namespace OmegaStore.Models;
 public partial class Blog
 {
     public int Id { get; set; }
-    public string Title { get; set; } = null!;
+	public string Author { get; set; } = null!;
+	public string Title { get; set; } = null!;
     public string Thumbnail { get; set; } = null!;
     public string Slug { get; set; } = null!;
-    public string Content { get; set; } = null!;
+	public string ListContent { get; set; } = null!;
+	public string ShortContent { get; set; } = null!;
+	public string Content { get; set; } = null!;
     public DateTime? CreatedAt { get; set; } = DateTime.Now;
 }
 
