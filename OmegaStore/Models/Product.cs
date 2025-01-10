aa644f3ed353513acc41bb77642d66de59551c08
NTPhat004 <ntphat05102004@gmail.com>
2025-01-10 19:31:43 +0700

    [Phat][Add New Product]

--- OmegaStore/Models/Product.cs
+++ OmegaStore/Models/Product.cs
@@ -9,13 +9,26 @@ public partial class Product
 {
     public int Id { get; set; }
     public string ProductCode { get; set; } = null!;
+
+    [Required(ErrorMessage = "Tên sản phẩm là bắt buộc.")]
+    [StringLength(255, ErrorMessage = "Tên sản phẩm không được vượt quá 255 ký tự.")]
     public string Name { get; set; } = null!;
     public string Thumbnail { get; set; } = null!;
+
+    [Required(ErrorMessage = "Tỷ lệ giảm giá phải nằm trong khoảng từ 0 đến 100."), Range(0, double.MaxValue, ErrorMessage = "Giá phải lớn hơn hoặc bằng 0.")]
     public decimal Price { get; set; }
+
+    
+    [Required(ErrorMessage = "Tỷ lệ giảm giá phải nằm trong khoảng từ 0 đến 100."), Range(0, 100, ErrorMessage = "Tỷ lệ giảm giá phải nằm trong khoảng từ 0 đến 100.")]
     public int DiscountRate { get; set; }
+
     public string Slug { get; set; } = null!;
+
+    [Required,Range(0,int.MaxValue,ErrorMessage = "Số lượng tồn kho là bắt buộc.")]
     public int Stock { get; set; }
     public string? Description { get; set; }
+
+    [Required,Range(1,int.MaxValue,ErrorMessage = "Danh mục là bắt buộc.")]
     public int CategoryId { get; set; }
     public int Status { get; set; }
 
