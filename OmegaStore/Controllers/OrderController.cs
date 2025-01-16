caf9f1bf9edb11b0ed3298b4a1b7d95db2c58ed2
NguyenTu2004-Work <nguyenduythanhtu2004@gmail.com>
2025-01-16 21:54:06 +0700

    [ThanhTu][OrderDetail][Customer]

--- OmegaStore/Controllers/OrderController.cs
+++ OmegaStore/Controllers/OrderController.cs
@@ -1,12 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OmegaStore.Models;
+using OmegaStore.Models.ViewModels;
+using OmegaStore.Services;
 
 namespace OmegaStore.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Index()
+        private readonly StoreDbContext _context;
+
+        public OrderController(IAccountService accountService, StoreDbContext context)
         {
-            return View();
+         
+            _context = context;
+        }
+        public IActionResult Index(int id)
+        {
+            //var CheckOrderDetail = _context.Orders
+            //      .Where(o => o.Id == id)
+            //    .Include(o => o.DetailOrders)
+            //    .ThenInclude(p => p.Product).ToList();
+          
+
+
+            // Thông tin chi tiết đơn hàng
+                var OrderDetail = _context.Orders
+                  .Where(o => o.Id == id)
+                .Include(o => o.DetailOrders)
+                .ThenInclude(p => p.Product).Select(p => new OrderDetailCustomerViewModel
+                {
+                    OrderCodeId = p.OrderCode,
+                    Note=p.Note,
+                    Crreate_at = p.CreatedAt,
+                    StatusOrder = p.Status,
+                    Name = p.Fullname,
+                    PhoneNumber = p.Phone,
+                    EmailUser = p.Email,
+                    AddressUser = p.Address,
+                    Total=p.TotalAmount,
+                }).FirstOrDefault();
+
+          
+           
+
+            //var CheckProductListOrderDetail = _context.DetailOrders
+            //                     .Where(d => d.OrderId == id)
+            //                    .Include(d => d.Product).ToList();
+
+
+           // Thông tin chi tiết danh sách sản phẩm đơn hàng
+                var ProductListOrderDetail = _context.DetailOrders
+                                 .Where(d => d.OrderId == id)
+                                .Include(d => d.Product).Select(p => new OrderDetailProductListCustomerViewModel
+                                {
+                                    ProductSlug=p.Product.Slug,
+                                    Thumbnail = p.Product.Thumbnail,
+                                    Name = p.Product.Name,
+                                    Price = p.Product.Price,
+                                    DiscountRate = p.Product.DiscountRate,
+                                    Quantity = p.Quantity,
+                                    TotalPrice = p.TotalPrice,
+
+                                }).ToList();
+
+        
+
+            var ViewModelDetailOrder = new OrderDetailCustomerAllViewModel()
+            {
+                OrderDetailCustomerViewModel = OrderDetail,
+                OrderDetailProductListCustomerViewModels = ProductListOrderDetail.Any() ? ProductListOrderDetail : null
+
+			};
+            return View(ViewModelDetailOrder);
         }
     }
 }
